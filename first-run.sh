c191452248e5a31a5c56f92b982c7f9fbbe0be38
Shane Mendez <mendezsoftware@gmail.com>
2022-04-02 00:04:01 -0500

    added output

--- first-run.sh
+++ first-run.sh
@@ -14,3 +14,5 @@ melwalletd --wallet-dir ~/.themelio > melwalletd.log 2>&1 &
 sleep 2; curl -s localhost:11773/wallets/w
 
 melminter --backup-wallet w > melminter.log 2>&1 &
+
+echo "Installing the minter is complete"
